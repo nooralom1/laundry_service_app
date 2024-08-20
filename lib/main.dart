9967b6bf7dea5023f0fca7364f5d9dda5a3f4653
NS NooR <non377938@gmail.com>
2024-08-21 00:01:20 +0600

    Laundry

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:laundry_app_using_getx/view/screen/welcome/welcome.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Welcome(),
+    );
+  }
+}
