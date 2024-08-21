fc2d93faab14a7dd4bad106cf4c2fdb07ab5c810
NS NooR <non377938@gmail.com>
2024-08-22 00:04:38 +0600

    signin

---
+++ lib/view/screen/authentication/widget/MailField.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:laundry_app_using_getx/utils/constants/color.dart';
+
+class MailField extends StatelessWidget {
+  const MailField({
+    super.key, required this.nameController,
+  });
+  final TextEditingController nameController;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 48,
+      child: TextFormField(
+        decoration: InputDecoration(
+          hintText: "mister@gmail.com",
+          labelText: "E-mail",
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: const BorderSide(color: CommonColor.blue,width: 2)),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: const BorderSide(color: CommonColor.blue,width: 2)),
+          errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: const BorderSide(color: Colors.red,width: 2)),
+          focusedErrorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: const BorderSide(color: Colors.red,width: 2)),
+        ),
+        validator: (value) {
+          if( value == null || value == ""){
+            return "E-mail can't be emty !";
+          }else if(value.contains("@") || value.contains(".")){
+            return "Enter Valid E-mail !";
+          }
+          return null;
+        },
+      ),
+    );
+  }
+}
