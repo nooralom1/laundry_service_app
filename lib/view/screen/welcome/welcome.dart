801f2e1a0a533117035062eb892f2113bb187849
NS NooR <non377938@gmail.com>
2024-08-22 00:03:02 +0600

    Welcome

--- lib/view/screen/welcome/welcome.dart
+++ lib/view/screen/welcome/welcome.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:laundry_app_using_getx/utils/constants/color.dart';
+import 'package:laundry_app_using_getx/view/common_widget/common_buttun.dart';
+import 'package:laundry_app_using_getx/view/common_widget/common_text.dart';
+import 'package:laundry_app_using_getx/view/screen/authentication/signin.dart';
+import 'package:laundry_app_using_getx/view/screen/authentication/signup.dart';
+import 'package:laundry_app_using_getx/view/screen/welcome/widget/login_button.dart';
 
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
@@ -6,26 +13,71 @@ class Welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: CommonColor.bgColor,
       body: Stack(
         fit: StackFit.expand,
         children: [
           Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset(
-                "assets/images/logincircleavatar.png",
+              const SizedBox(
+                height: 150,
               ),
-              Container(
+              Image.asset(
+                "assets/images/welcomecircleavatar.png",
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              CommonButton(
                 height: 46,
                 width: 318,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: const Color(0xff0A51EB)
-                ),
-              )
+                btnName: 'Create account',
+                onTap: () {
+                  Get.to(()=>const Signup());
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              LoginButton(
+                onTap: () {
+                  Get.to(()=>const Signin());
+                },
+              ),
             ],
           ),
-          Image.asset("assets/images/splash.png")
+          Positioned(
+            top: 185,
+            left: 35,
+            child: Column(
+              children: [
+                Image.asset("assets/images/splash.png"),
+                const CommonText(
+                  tittle: "Laundry Service",
+                  fWeight: FontWeight.bold,
+                  fSize: 36,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const CommonText(
+                  tittle: "One stop destination for all a laundry work",
+                  fWeight: FontWeight.w600,
+                  fSize: 15,
+                ),
+                const CommonText(
+                  tittle: "such as washing, ironing and dry",
+                  fWeight: FontWeight.w600,
+                  fSize: 15,
+                ),
+                const CommonText(
+                  tittle: "cleaning",
+                  fWeight: FontWeight.w600,
+                  fSize: 15,
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
