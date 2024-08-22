f1c4c5df69d1435548312cb3b4d6d6e8fafc2a2b
NS NooR <non377938@gmail.com>
2024-08-22 13:05:50 +0600

    home

--- lib/view/screen/home/widget/custom_card.dart
+++ lib/view/screen/home/widget/custom_card.dart
@@ -3,31 +3,35 @@ import 'package:laundry_app_using_getx/view/common_widget/common_text.dart';
 
 class CustomCard extends StatelessWidget {
   const CustomCard({
-    super.key,
+    super.key, required this.image, required this.name, required this.title,
   });
+  final String image;
+  final String name;
+  final String title;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 158,
       child: Card(
-        color: Colors.black38,
+        color: const Color(0xffa8c3d7),
         child: Center(
           child: Padding(
             padding: const EdgeInsets.only(left: 10),
             child: Row(
               children: [
-                Container(
+                SizedBox(
                   height: 107,
                   width: 132,
-                  color: Colors.red,
+                  child: Image.network(image),
                 ),
                 const SizedBox(width: 10,),
-                const Column(
+                 Column(
+                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    CommonText(tittle: "Daily use clothes",fSize: 25,fWeight: FontWeight.bold,),
-                    CommonText(tittle: "T-shirts,Shirts,Pants",fSize: 14,fWeight: FontWeight.w600,)
+                    CommonText(tittle: name,fSize: 22,fWeight: FontWeight.bold,),
+                    CommonText(tittle: title,fSize: 14,fWeight: FontWeight.w600,color: Colors.black,)
                   ],
                 )
               ],
--- lib/view/screen/home/widget/custom_container.dart
+++ lib/view/screen/home/widget/custom_container.dart
@@ -1,36 +1,33 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:laundry_app_using_getx/utils/constants/color.dart';
 import 'package:laundry_app_using_getx/view/common_widget/common_text.dart';
 
 class CustomContainer extends StatelessWidget {
   const CustomContainer({
-    super.key,
+    super.key, required this.image, required this.name,
   });
+  final String image;
+  final String name;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(right: 10),
-            child: SizedBox(
-              height: 114,
-              width: 114,
-              child: Card(
-                color: Colors.red,
-              ),
-            ),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 10),
+          child: SizedBox(
+            height: 114,
+            width: 114,
+            child: Image.network(image),
           ),
-          CommonText(
-            tittle: "Wash",
-            color: CommonColor.blue,
-            fSize: 21,
-            fWeight: FontWeight.bold,
-          )
-        ],
-      ),
+        ),
+        CommonText(
+          tittle: name,
+          color: CommonColor.blue,
+          fSize: 21,
+          fWeight: FontWeight.bold,
+        )
+      ],
     );
   }
 }
