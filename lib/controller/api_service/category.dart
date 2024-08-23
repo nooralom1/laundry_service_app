a6ba9a537cd8577d7c177ed5647ed5e79b4781b2
NS NooR <non377938@gmail.com>
2024-08-23 22:46:17 +0600

    modified

--- lib/controller/api_service/category.dart
+++ lib/controller/api_service/category.dart
@@ -3,13 +3,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:laundry_app_using_getx/database/category.dart';
 import 'package:laundry_app_using_getx/model/category.dart';
 
-class CategoryService{
-
-  static Future<CategoryModel?> categoryService()async{
-    try{
-      CategoryModel categoryModel = CategoryModel.fromJson(jsonDecode(jsonEncode(CategoryList.categoryList)));
+class CategoryService {
+  static Future<CategoryModel?> categoryService() async {
+    try {
+      CategoryModel categoryModel = CategoryModel.fromJson(
+          jsonDecode(jsonEncode(CategoryList.categoryList)));
       return categoryModel;
-    }catch(e){
+    } catch (e) {
       debugPrint("Error: $e");
     }
     return null;
