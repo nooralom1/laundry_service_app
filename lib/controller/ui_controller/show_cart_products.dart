cbfde5a732af68f06b32324e61911944794adfa2
NS NooR <non377938@gmail.com>
2024-08-26 23:47:11 +0600

    Show CArt

---
+++ lib/controller/ui_controller/show_cart_products.dart
@@ -0,0 +1,22 @@
+import 'package:get/get.dart';
+import 'package:laundry_app_using_getx/controller/api_service/show_cart_products.dart';
+import 'package:laundry_app_using_getx/model/show_cart_products.dart';
+
+class ShowCartProductsController extends GetxController{
+
+  RxBool isLoading = false.obs;
+  List<CartProducts> showCart= <CartProducts>[].obs;
+
+  getShowCart()async{
+    isLoading.value=true;
+    var data = await ShowCartProductsService.showCartProductsService();
+    isLoading.value=false;
+    showCart = data?.cartProducts??[];
+  }
+
+  @override
+  void onInit() {
+    getShowCart();
+    super.onInit();
+  }
+}
