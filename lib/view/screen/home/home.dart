fe6cf88659816cf5ada3d71eed510971fdd8f933
NS NooR <non377938@gmail.com>
2024-09-06 23:31:18 +0600

    modified

--- lib/view/screen/home/home.dart
+++ lib/view/screen/home/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:laundry_app_using_getx/controller/ui_controller/home.dart';
-import 'package:laundry_app_using_getx/utils/constants/color.dart';
+import 'package:laundry_app_using_getx/controller/getx_controller/home.dart';
+import 'package:laundry_app_using_getx/utils/app_color/color.dart';
 import 'package:laundry_app_using_getx/view/common_widget/common_text.dart';
 import 'package:laundry_app_using_getx/view/screen/category_details/category_details.dart';
 import 'package:laundry_app_using_getx/view/screen/home/widget/categoryviewcard.dart';
@@ -18,95 +18,106 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     HomeController homeController = Get.put(HomeController());
+    double screenHeight = MediaQuery.sizeOf(context).height;
+    double screenWidth = MediaQuery.sizeOf(context).width;
     return Scaffold(
       backgroundColor: CommonColor.bgColor,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      CommonText(
-                        tittle: "Home",
-                        fWeight: FontWeight.w800,
-                        fSize: 22,
-                      ),
-                      CommonText(
-                          tittle: "Block no.23,Saigaon,0043",
-                          color: CommonColor.blue,
-                          fSize: 17)
-                    ],
-                  ),
-                  CircleAvatar(
-                    radius: 28,
-                    child: Center(
-                      child: Icon(
-                        Icons.notifications,
-                        color: CommonColor.blue,
-                        size: 35,
-                      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: screenHeight * 0.065,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child:  Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const CommonText(
+                      tittle: "Home",
+                      fWeight: FontWeight.w800,
+                      fSize: 22,
                     ),
-                  )
-                ],
+                    CommonText(
+                        tittle: "Block no.23,Saigaon,0043",
+                        color: CommonColor.blue,
+                        fSize: screenWidth*0.043,
+                      overflow: TextOverflow.ellipsis,)
+                  ],
+                ),
+                const CircleAvatar(
+                  radius: 28,
+                  child: Center(
+                    child: Icon(
+                      Icons.notifications,
+                      color: CommonColor.blue,
+                      size: 35,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: screenHeight * 0.03,
+          ),
+          Obx(
+            () => SizedBox(
+              height: screenHeight * 0.22,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: homeController.demoData.length,
+                    itemBuilder: (context, index) {
+                      return DemoViewCard(
+                        image: ("${homeController.demoData[index].image}"),
+                        name: ("${homeController.demoData[index].name}"),
+                      );
+                    }),
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              Obx(
-                () => SizedBox(
-                  height: 150,
-                  child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: homeController.demoData.length,
-                      itemBuilder: (context, index) {
-                        return DemoViewCard(
-                          image: ("${homeController.demoData[index].image}"),
-                          name: ("${homeController.demoData[index].name}"),
-                        );
-                      }),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                CommonText(
+                  tittle: "Category",
+                  fWeight: FontWeight.bold,
+                  fSize: 25,
+                  color: CommonColor.blue,
+                ),
+              ],
+            ),
+          ),
+          Obx(
+            () => Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 8),
+                child: ListView.builder(
+                  itemCount: homeController.categories.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: CategoryViewCard(
+                        image: ("${homeController.categories[index].image}"),
+                        name: ("${homeController.categories[index].name}"),
+                        title: ("${homeController.categories[index].tittle}"),
+                        onTap: () {
+                          Get.to(() => const CategoryDetailsPage());
+                        },
+                      ),
+                    );
+                  },
                 ),
               ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  CommonText(
-                    tittle: "Category",
-                    fWeight: FontWeight.bold,
-                    fSize: 25,
-                    color: CommonColor.blue,
-                  ),
-                ],
-              ),
-              Obx(() => SizedBox(
-                  height: MediaQuery.sizeOf(context).height,
-                  child: ListView.builder(
-                      itemCount: homeController.categories.length,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.only(bottom: 10),
-                          child: CategoryViewCard(
-                            image:
-                                ("${homeController.categories[index].image}"),
-                            name: ("${homeController.categories[index].name}"),
-                            title:
-                                ("${homeController.categories[index].tittle}"),
-                            onTap: () {
-                              Get.to(() => const CategoryDetailsPage());
-                            },
-                          ),
-                        );
-                      })))
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
